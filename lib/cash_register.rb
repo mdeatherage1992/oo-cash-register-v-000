c25ce2e690a5f548a3f47f78c6d4ee5df25fd2f4
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-20 13:19:42 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,11 @@
 class CashRegister
 attr_accessor
+
   def initialize
     @total = 0
-    @employee_discount = employee_discount
   end
-  def add_item(item,price)
-    self.total += price
-  end
+
+
 
 
 
