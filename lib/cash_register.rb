f252a084f789f8c79c60023ce95f2d2d6f788b6f
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-20 13:25:01 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@ attr_accessor :cash_register,:cash_register_with_discount
 
   def initialize
     @total = 0
-    @cash_register_with_discount.discount = 20
+    @cash_register_with_discount = 20
   end
 
 
